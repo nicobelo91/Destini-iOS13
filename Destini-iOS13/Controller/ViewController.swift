d1d9fb32cdb4b7fb4e845afe204a4a9d1d576e82
Nico Cobelo <ingcobelo@gmail.com>
2020-10-25 15:50:25 -0400

    Used MVC to organize the code, added functions and structs

--- Destini-iOS13/Controller/ViewController.swift
+++ Destini-iOS13/Controller/ViewController.swift
@@ -14,11 +14,32 @@ class ViewController: UIViewController {
     @IBOutlet weak var choice1Button: UIButton!
     @IBOutlet weak var choice2Button: UIButton!
     
+    var storyBrain = StoryBrain()
+                   
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+       updateUI()
+        //loads the first title in the array
     }
 
-
+    @IBAction func choiceMade(_ sender: UIButton) {
+        
+        let userAnswer = sender.currentTitle!
+        storyBrain.nextStory(userAnswer) 
+        updateUI()
+    }
+    
+    func updateUI() {
+        //update the storyLabel text
+        //update the choice1Button title
+        //update the choice2Button title
+        
+        storyLabel.text = storyBrain.nextTitle()
+        choice1Button.setTitle(storyBrain.nextchoice1(), for: .normal)
+        choice2Button.setTitle(storyBrain.nextchoice2(), for: .normal)
+    }
+    
 }
 
